f873278093b7bdfd59dc3ba3dc9871ad0726f9d7
Bruno Garcia <brunogalberti@gmail.com>
2025-02-21 16:19:18 -0300

    features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import Features from "@/sections/Features";
+import Hero from "@/sections/Hero";
+import Introduction from "@/sections/Introduction";
+import LogoTicker from "@/sections/LogoTicker";
 import Navbar from "@/sections/Navbar";
 
 export default function Home() {
-    return <><Navbar /></>;
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <LogoTicker />
+            <Introduction />
+            <Features />
+        </>
+    );
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,55 @@
+import Button from "@/components/Button";
+
+import designExample1 from "@/assets/images/design-example-1.png";
+import designExample2 from "@/assets/images/design-example-2.png";
+import Image from "next/image";
+import Pointer from "@/components/Pointer";
+
 export default function Hero() {
-    return <div>Hero</div>;
+    return (
+        <section className="py-24 overflow-x-clip">
+            <div className="container relative">
+                <div className="absolute -left-32 top-16 hidden lg:block">
+                    <Image src={designExample1} alt="Desing example 1 Image" />
+                </div>
+                <div className="absolute -right-64 -top-16 hidden lg:block">
+                    <Image src={designExample2} alt="Desing example 2 Image" />
+                </div>
+                <div className="absolute left-56 top-96">
+                    <Pointer name="Andrea" />
+                </div>
+                <div className="absolute right-80 -top-4">
+                    <Pointer name="Brain" color="red" />
+                </div>
+                <div className="flex justify-center">
+                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
+                        ✨ $7,5 millones de dólares recaudados
+                    </div>
+                </div>
+                <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">
+                    Diseño impactante, creado sin esfuerzo.
+                </h1>
+                <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
+                    Las herramientas de diseño no deberían ralentizarte. Layers
+                    combina potentes funciones con una interfaz intuitiva que te
+                    mantiene en tu flujo creativo.
+                </p>
+                <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
+                    <input
+                        type="email"
+                        placeholder="Ingresa tu Email"
+                        className="bg-transparent px-4 md:flex-1"
+                    />
+                    <Button
+                        variant="primary"
+                        size="sm"
+                        type="submit"
+                        className="whitespace-nowrap"
+                    >
+                        Registrarse
+                    </Button>
+                </form>
+            </div>
+        </section>
+    );
 }
